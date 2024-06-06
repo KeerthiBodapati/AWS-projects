8012b9e43f4ea07420fda29257c910e74d31ebda
KeerthiBodapati <155158922+KeerthiBodapati@users.noreply.github.com>
2024-06-06 22:20:36 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -e
 
-echo "Stopping container..."
-# Get the container ID of the running container, ignoring the header line
-container_id=$(docker ps --format "{{.ID}}" | awk 'NR==1 {next} NR==2 {print $1}')
-
-if [ -n "$container_id" ]; then
-      docker rm -f $container_id
-       echo "Container $container_id stopped and removed."
-else
-    echo "No running container found."
-fi
-     
+# Stop the running container (if any)
+echo "Hi"
