68ed81cbd8fbf73661a9ef8fe5be7505cfa6ea45
KeerthiBodapati <155158922+KeerthiBodapati@users.noreply.github.com>
2024-06-06 16:30:55 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Stop the running container (if any)
-Containerid= 'docker ps | awk -F " " '{PRINT $1}''
+Containerid= 'docker ps | awk -F " " '{PRINT $1}'' 
 
 #removing existing container
 docker rm -f $Containerid
