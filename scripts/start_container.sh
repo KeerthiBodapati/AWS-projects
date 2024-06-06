8327bdbb8cf111f3e696cb74af9c10bd1bf993ce
KeerthiBodapati <155158922+KeerthiBodapati@users.noreply.github.com>
2024-06-06 16:05:07 +0530

    start container

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull keerthid2001/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 keerthid2001/simple-python-flask-app:latest 
